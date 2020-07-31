52d27225b665ad0722a6f8fe5b7f20f613d35ac8
tetsu-upstr <bodomu@gmail.com>
2020-08-01 06:30:21 +0900

    dispose

---
+++ CS24/Database.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+
+namespace CS24
+{
+
+    public class Database : IDisposable
+    {
+        // 全体で使いたい時
+        private SqlDataAdapter _adapter = new SqlDataAdapter();
+        public Database()
+        {
+        
+        }
+
+        public void Insert()
+        {
+            // SqlConnection connection = new SqlConnection();
+
+            // try
+            // {
+            //     // 処理
+            // }
+            // catch(Exception ex)
+            // {
+
+            // }
+            // finally // finlallyを宣言すると最後に必ず通る
+            // {
+            //     // Dispose リソースの開放宣言
+            //     connection.Dispose();
+            // }
+
+            // usingを使うDisposeの方法
+            using (SqlConnection connection = new SqlConnection())
+            using (SqlCommand command = new SqlCommand("select * from xxx", connection))
+            {
+                // 処理
+            }
+        }
+
+        public void Cancel()
+        {
+            _adapter.SelectCommand.Cancel();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementException();
+        }
+
+    }
+
+}
