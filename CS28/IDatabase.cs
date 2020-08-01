1dbf17672beed2e4396e102af3b65512ab7a795b
tetsu-upstr <bodomu@gmail.com>
2020-08-02 05:22:14 +0900

    Interface

---
+++ CS28/IDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace CS28
+{
+    // ポリモーフィズム　インターフェイス Iから始める命名
+    interface IDatabase
+    {
+        void Save();
+    }
+
+    
+}
---
+++ CS28/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CS28
+{
+    class Products : IDatabase
+    {
+        public void Save()
+        {
+            Console.WriteLine("商品マスタの登録");
+        }
+    }
+    
+}
