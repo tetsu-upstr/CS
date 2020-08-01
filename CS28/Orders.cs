84adf518f7dbd4a71d17c855f3c6d32a983dbbde
tetsu-upstr <bodomu@gmail.com>
2020-08-02 05:29:52 +0900

    add Orders

---
+++ CS28/Orders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS28
+{
+    public class Orders : IDatabase
+    {
+        public void Save()
+        {
+            Console.WriteLine("発注テーブル登録");
+        }
+    }
+}
