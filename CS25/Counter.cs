c0db23c899a4f5e9ddd3116c12366b96b300d2e4
tetsu-upstr <bodomu@gmail.com>
2020-08-01 13:45:46 +0900

    static

---
+++ CS25/Counter.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+namespace CS25
+{
+    internal class Counter
+    {
+        // 静的メンバー
+        // クラス、変数にstaticをつける
+        // クラス単位で生成 = アプリケーションにただ一つ
+        private static int _count = 0;
+        private int _value = 0;
+
+        // 静的メソッド
+        internal static void Call()
+        {
+            // 処理の中で使えるのは静的メンバーのみ
+            _count++;
+            // sitaticじゃない変数は呼び出せない
+        }
+
+        // new せずにそのまま呼び出せる
+        // Windows formなどで Counter.Call()として使用する
+
+        internal static int GetCount()
+        {
+            return _count;
+        }
+    }
+
+    
+}
