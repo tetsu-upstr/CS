387e89f6a9f59811332287deb304d5d3f772036d
tetsu-upstr <bodomu@gmail.com>
2020-08-01 14:35:25 +0900

    override

---
+++ CS27/A1.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+namespace CS27
+{
+    class A1
+    {
+        // スーパークラス 親クラス（基底クラス)
+        private int _privateValue;
+        protected int protectedValue;
+
+        // コンストラクタ
+        public A1(int value)
+        {
+            _privateValue = value;
+        }
+
+        // 基底クラス　virtual で オーバーライドしてもいいよのサイン
+        public virtual int GetData()
+        {
+            return 1;
+        }
+    }
+
+    
+}
